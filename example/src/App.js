eb1afcde77010adcdf1eeccf65287c35859bca46
Alejandro Leonian <alejandromleonian@gmail.com>
2024-01-04 16:30:19 -0300

    modified example

--- example/src/App.js
+++ example/src/App.js
@@ -15,32 +15,45 @@ function App() {
   return (
     <Fragment>
       <h3> Some cards</h3>
-      <Ace suit="club" />
-      <Ace suit="club" />
-      <Two suit="heart" />
-      <Three suit="spade" />
-      <Four suit="diamond" />
-      <Back />
-      <Joker />
+      <div className='poker-table'>
+
+        <Ace suit="club" />
+        <Ace suit="club" />
+        <Two suit="heart" />
+        <Three suit="spade" />
+        <Four suit="diamond" />
+        <Back />
+        <Joker />
+      </div>
+
       <h3> A whole suit</h3>
-      <ASuit suit="spade" />
+      <div className='poker-table'>
+
+        <ASuit suit="spade" />
+      </div>
+
       <h3> Other cards obtained via 'getCard()'</h3>
-      {getCard("ad")}
-      {getCard("2c")}
-      {getCard("03d")}
-      {getCard("4s")}
-      {getCard("05h")}
-      {getCard("6s")}
-      {getCard("7c")}
-      {getCard("08d")}
-      {getCard("9h")}
-      {getCard("10s")}
-      {getCard("jc")}
-      {getCard("12s")}
-      {getCard("kh")}
-      {getCard("ah")}
+      <div className='poker-table'>
+
+        {getCard("ad")}
+        {getCard("2c")}
+        {getCard("03d")}
+        {getCard("4s")}
+        {getCard("05h")}
+        {getCard("6s")}
+        {getCard("7c")}
+        {getCard("08d")}
+        {getCard("9h")}
+        {getCard("10s")}
+        {getCard("jc")}
+        {getCard("12s")}
+        {getCard("kh")}
+        {getCard("ah")}
+      </div>
       <h3> The full deck!</h3>
-      <FullDeck />
+      <div className='poker-table'>
+        <FullDeck />
+      </div>
     </Fragment>
   );
 }
